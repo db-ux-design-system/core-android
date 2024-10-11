46eefdf5b522abb60e15cf591a7a28988d72ab6c
Thomas Köcher <thomas.koecher@deutschebahn.com>
2024-10-11 17:20:52 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# db-ux-design-system-android
+# DB UX Design System Compose Library
+
+Welcome to the UX Design System Library project for Jetpack Compose!
+
+For detailed development guidelines, please refer to our [DEVELOPMENT.md](./DEVELOPMENT.md) document.
+
+
+## License
+
+This project is licensed under [Apache-2.0](LICENSE), Copyright 2024 by DB Systel GmbH.
---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.dbsystel.dbuxds"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 30
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
