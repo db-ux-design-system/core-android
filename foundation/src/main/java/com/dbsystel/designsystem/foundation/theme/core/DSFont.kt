c4f6036a952f37feb5cf6d8fe2fdae9bab0604c2
Thomas Köcher <thomas.koecher@deutschebahn.com>
2024-10-23 16:34:57 +0200

    Remove font reference for now

--- foundation/src/main/java/com/dbsystel/designsystem/foundation/theme/core/DSFont.kt
+++ foundation/src/main/java/com/dbsystel/designsystem/foundation/theme/core/DSFont.kt
@@ -1,12 +1,9 @@
 package com.dbsystel.designsystem.foundation.theme.core
 
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import com.dbsystel.designsystem.foundation.R
 
 internal object DSFont {
     val dsNeoScreenFlex = FontFamily(
-        Font(R.font.db_neo_screen_flex, FontWeight.Normal)
+        //  Font(R.font.db_neo_screen_flex, FontWeight.Normal)
     )
 }
