fbd6fd40eae4eb52728b12d1dd437ffa5eb31293
Fabian Wenzel <fabian.wenzel@deutschebahn.com>
2024-11-19 09:49:34 +0100

    Feature/ds card component (#5)
    * Added DSCard Component
    
    * [ADDED] DSCard elevation and spacing parameter
    
    * added: preview libs
    
    * rework: pullrequest comments
    
    ---------
    
    Co-authored-by: Stefan Ankele <stefan.ankele@deutschebahn.com>

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -37,13 +37,20 @@ android {
 }
 
 dependencies {
+    implementation(project(":foundation"))
+
+    // Android Studio Preview support
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.material3)
-    implementation(project(":foundation"))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
