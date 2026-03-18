5ad29f58a6b82cc95c57ab500610eefa759b300b
Thomas Köcher <thomas.koecher@deutschebahn.com>
2026-03-18 17:49:05 +0100

    Added encrypted font and action to decrypt it in future workflows. Added build workflow

--- foundation/src/main/java/com/dbsystel/designsystem/foundation/theme/core/DBFont.kt
+++ foundation/src/main/java/com/dbsystel/designsystem/foundation/theme/core/DBFont.kt
@@ -1,8 +1,11 @@
 package com.dbsystel.designsystem.foundation.theme.core
 
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import com.dbsystel.designsystem.foundation.R
 
 internal object DBFont {
     val dbNeoScreenFlex = FontFamily(
+        Font(R.font.db_neo_screen_flex)
     )
 }
