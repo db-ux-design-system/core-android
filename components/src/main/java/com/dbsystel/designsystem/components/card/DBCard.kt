172f453ad2f6b93301ff215d7301e9cb062f16e8
Thomas Köcher <89777337+tkoecher@users.noreply.github.com>
2026-04-13 16:34:58 +0200

    feat: Added interactive behavior for DBCard (#17)

--- components/src/main/java/com/dbsystel/designsystem/components/card/DBCard.kt
+++ components/src/main/java/com/dbsystel/designsystem/components/card/DBCard.kt
@@ -1,7 +1,11 @@
 package com.dbsystel.designsystem.components.card
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -12,14 +16,18 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dbsystel.designsystem.components.card.extensions.color
+import com.dbsystel.designsystem.components.card.extensions.colorPressed
 import com.dbsystel.designsystem.components.card.extensions.padding
 import com.dbsystel.designsystem.components.card.preview.previewName
 import com.dbsystel.designsystem.components.card.preview.previewNameShort
@@ -35,6 +43,9 @@ import com.dbsystel.designsystem.foundation.theme.DBTheme
  * @param modifier Modifier to be applied to the card.
  * @param elevation The elevation level of the card, which determines its background color.
  * @param spacing The spacing level of the card, which determines the padding inside the card.
+ * @param behavior The behavior of the card, which determines whether it is static or interactive.
+ * @param onClick The callback to be invoked when the card is clicked. The card is only clickable if
+ * the behavior is set to interactive and onClick is not null.
  * @param content The content of the card in a ColumnScope as a receiver.
  *
  * @sample com.dbsystel.designsystem.components.card.preview.DBCardSample
@@ -44,22 +55,38 @@ fun DBCard(
     modifier: Modifier = Modifier,
     elevation: DBCardElevation = DBCardElevation.LEVEL_1,
     spacing: DBCardSpacing = DBCardSpacing.SMALL,
+    behavior: DBCardBehavior = DBCardBehavior.STATIC,
+    onClick: (() -> Unit)? = null,
     content: @Composable ColumnScope.() -> Unit,
 ) {
     val shape = RoundedCornerShape(DBTheme.dimensions.border.radiusSm)
-
+    val interactionSource = remember { MutableInteractionSource() }
+    val pressed by interactionSource.collectIsPressedAsState()
+    val backgroundColor by animateColorAsState(
+        targetValue = if (pressed) elevation.colorPressed else elevation.color,
+        label = "CardBackgroundColoAnimation"
+    )
     Surface(
-        color = elevation.color,
+        color = backgroundColor,
+        shape = shape,
         contentColor = DBTheme.activeColor.Basic.Text.Default.Default,
         tonalElevation = 0.dp,
         modifier = modifier
-            .background(color = elevation.color, shape = shape)
             .border(
                 width = DBTheme.dimensions.border.width3xs,
                 color = DBTheme.activeColor.Basic.Border.Default.Default,
                 shape = shape,
             )
-            .clip(shape = shape),
+            .then(
+                if (behavior == DBCardBehavior.INTERACTIVE && onClick != null) {
+                    Modifier.clickable(
+                        role = Role.Button,
+                        interactionSource = interactionSource,
+                        indication = ripple(color = elevation.colorPressed),
+                        onClick = { onClick() },
+                    )
+                } else Modifier
+            ),
     ) {
         Column(modifier = Modifier.padding(spacing.padding)) {
             content()
@@ -80,7 +107,12 @@ enum class DBCardElevation {
     LEVEL_3;
 }
 
-@Preview
+enum class DBCardBehavior {
+    STATIC,
+    INTERACTIVE;
+}
+
+@Preview(heightDp = 1000)
 @Composable
 private fun DBCardPreview() {
     val cardSize = 90.dp
@@ -117,6 +149,23 @@ private fun DBCardPreview() {
                                     color = Color(0xFFD600DB),
                                 )
                             }
+                        }
+                    }
+                }
+            ),
+            BasePreviewProperties(
+                property = "Behavior",
+                views = DBCardBehavior.entries.map { behavior ->
+                    behavior.previewName to {
+                        DBCard(
+                            modifier = Modifier.size(cardSize),
+                            behavior = behavior,
+                            onClick = {},
+                        ) {
+                            Box(
+                                contentAlignment = Alignment.Center,
+                                modifier = Modifier.fillMaxSize(),
+                            ) { }
                         }
                     }
                 }
